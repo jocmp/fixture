67cff14cbd01681c492da4aaf90e1fcf7960b9d4
Josiah Campbell <jocmp64@gmail.com>
2019-04-17 12:52:17 -0500

    Tests passing

---
+++ src/ast.ts
@@ -0,0 +1,98 @@
+// apollo-codegen uses Object.entries and Array.flatmap
+import 'core-js/es/object';
+import 'core-js/es/array';
+
+import {DocumentNode, GraphQLInputType, GraphQLSchema} from 'graphql';
+import {
+  BooleanCondition,
+  CompilerOptions,
+  compileToLegacyIR as compileToIR,
+  LegacyCompilerContext,
+  LegacyField,
+  LegacyFragment,
+  LegacyInlineFragment,
+  LegacyOperation,
+} from 'apollo-codegen-core/lib/compiler/legacyIR';
+
+export enum OperationType {
+  Query = 'query',
+  Mutation = 'mutation',
+  Subscription = 'subscription',
+}
+
+export interface Variable {
+  name: string;
+  type?: GraphQLInputType;
+}
+
+export interface TypedVariable extends Variable {
+  type: GraphQLInputType;
+}
+
+export interface PrintableFieldDetails {
+  fields?: Field[];
+  fragmentSpreads?: string[];
+  inlineFragments?: InlineFragment[];
+}
+
+export interface Condition extends BooleanCondition {
+  kind: string;
+}
+
+export interface Field extends LegacyField {
+  conditions?: Condition[];
+  fields?: Field[];
+  inlineFragments?: InlineFragment[] & {
+    [typeName: string]: InlineFragment;
+  };
+}
+
+export interface InlineFragment extends LegacyInlineFragment {
+  fields: Field[];
+}
+
+export interface Fragment extends LegacyFragment {
+  fields: Field[];
+  inlineFragments: InlineFragment[] & {
+    [typeName: string]: InlineFragment;
+  };
+}
+
+export interface Operation extends LegacyOperation {
+  fields: Field[];
+  variables: TypedVariable[];
+  inlineFragments?: InlineFragment[] & {
+    [typeName: string]: InlineFragment;
+  };
+}
+
+export interface AST extends LegacyCompilerContext {
+  operations: {
+    [operationName: string]: Operation;
+  };
+  fragments: {
+    [fragmentName: string]: Fragment;
+  };
+}
+
+export function isOperation(
+  operationOrFragment: Operation | Fragment,
+): operationOrFragment is Operation {
+  return (operationOrFragment as any).hasOwnProperty('operationName');
+}
+
+export function isTypedVariable(
+  variable: Variable | TypedVariable,
+): variable is TypedVariable {
+  return variable.type != null;
+}
+
+export interface Compile {
+  (
+    schema: GraphQLSchema,
+    document: DocumentNode,
+    options?: CompilerOptions,
+  ): AST;
+}
+
+export const compile = compileToIR as Compile;
